33ad6c0c0b2a2e556dd6e3a781a5fbfcf25371e3
Joao Correia <aluno21302@ipt.pt>
2022-08-09 17:32:32 +0100

    Criação do Model Camisola

---
+++ KitStore/Models/Camisola.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KitStore.Models
+{
+    public class Camisola
+    {
+        [Key]
+        public int Id { get; set; }
+        public string ImagemURL { get; set; }
+
+        public string NomeEquipa { get; set; }
+
+        public string NomeMarca { get; set; }
+
+        public string Equipamento { get; set; }
+    }
+}
